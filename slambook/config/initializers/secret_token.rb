36e00d5c7e991f526cd576f3a06926d206ba9ada
Sean O'Neill <seanon414@gmail.com>
2014-02-07 00:23:34 -0500

    Adding base ruby code

---
+++ slambook/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Slambook::Application.config.secret_key_base = '2367ba32540249174bee87c837cfa228fbf0962cc9409926b84e7e9040fd31208b349047c7b942bebafaa3924625bcc1441a8a43f5d27264f88283b6740c64d4'
